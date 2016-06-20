3bd7b35624283de6812df1fa5431d48bdf5c3e84
Erik <eschneider1128@gmail.com>
2016-06-20 12:09:31 -0400

    added sessions and logout route

--- src/main/java/com/theironyard/HelloSpringController.java
+++ src/main/java/com/theironyard/HelloSpringController.java
@@ -5,19 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by Erik on 6/20/16.
  */
 @Controller
 public class HelloSpringController {
-    User user = null;
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
 
     //public String home(Model model) non query parameters {
     //Person p = new Person("Alice", "Charleston", 30);
 
-    public String home (Model model, String name, String city, Integer age) { //with query parameters
+    public String home (Model model, HttpSession session, String name, String city, Integer age) { //with query parameters
 
         if (name == null) {
             name = "Erik";
@@ -30,12 +31,24 @@ public class HelloSpringController {
         }
         Person p = new Person(name, city, age);
         model.addAttribute("person", p);
+
+        String username = (String) session.getAttribute("username"); //need to cast
+        User user = null;
+        if (username != null) {
+            user = new User(username);
+        }
         model.addAttribute("user", user);
+
         return "person";
     }
     @RequestMapping(path = "/login", method = RequestMethod.POST)
-    public String login(String username) {
-        user = new User(username);
+    public String login(String username, HttpSession session) {
+        session.setAttribute("username", username);
+        return "redirect:/";
+    }
+    @RequestMapping(path = "/logout", method = RequestMethod.POST)
+    public String logout (HttpSession session) {
+        session.invalidate();
         return "redirect:/";
     }
 }
